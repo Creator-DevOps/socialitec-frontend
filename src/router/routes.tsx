fcd6c16270074a5a5af5fe35a37b36689eacb9cf
BUSTAMANTE <bustamanteservincarloseduardo@gmail.com>
2025-04-02 12:01:26 -0600

    Update routes.tsx

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -6,11 +6,11 @@ import SocialITEC from "@/components/ui-componets/general/SocialITEC";
 import R404 from "@pages/404";
 
 const routesConfig = [
-  { path: "login", element: <Login /> },  
-  { path: "student", element: <Student/> },
-  { path: "tecnm", element: <TecNM/> },
-  { path: "social-service", element: <SocialService/> },
-  { path: "socialitec", element: <SocialITEC/> },
+  { path: "/login", element: <Login /> },  
+  { path: "/student", element: <Student/> },
+  { path: "/tecnm", element: <TecNM/> },
+  { path: "/social-service", element: <SocialService/> },
+  { path: "/socialitec", element: <SocialITEC/> },
   { path: "*", element: <R404 /> },
 ];
 
