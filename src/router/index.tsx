1b2d198be8b52d0df4f03d121d55812932c52ba6
BUSTAMANTE <bustamanteservincarloseduardo@gmail.com>
2025-04-02 12:00:54 -0600

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,12 @@
-import { Routes, Route, Navigate, useLocation, useNavigate } from "react-router-dom";
-import routesConfig from "./routes";
-import R404 from "@/pages/404";
-import { useEffect } from "react";
-
 const AppRoutes = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const { pathname, search } = location;
-    const hasTrailingSlash = pathname.length > 1 && pathname.endsWith("/");
-    const segments = pathname.split("/").filter(Boolean);
-
-    if (hasTrailingSlash && segments.length === 1) {
-      const newPath = pathname.replace(/\/+$/, ""); 
-      navigate(newPath + search, { replace: true });
+    const { pathname, search, hash } = location;
+    if (pathname.length > 1 && pathname.endsWith("/")) {
+      const newPath = pathname.replace(/\/+$/, "");
+      navigate(`${newPath}${search}${hash}`, { replace: true });
     }
   }, [location, navigate]);
 
@@ -22,13 +14,11 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Navigate replace to="/login" />} />
       {routesConfig.map(({ path, element }) => (
-        <Route key={path} path={path} element={element} />
+        <Route key={path} path={`/${path}`} element={element} />
       ))}
       <Route path="*" element={<R404 />} />
     </Routes>
   );
 };
-
-export default AppRoutes;
 
 
