8f88cd12dc5bb7ee629a0f0f8a3578737962cc9d
BUSTAMANTE2034 <bustamanteservincarloseduardo@gmail.com>
2025-04-02 09:16:14 -0600

    redirección

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,25 @@
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import routesConfig from "./routes";
 import R404 from "@/pages/404";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AppRoutes = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const hasTrailingSlash =
+      location.pathname.length > 1 && location.pathname.endsWith("/");
+    if (hasTrailingSlash) {
+      const newPath = location.pathname.replace(/\/+$/, "");
+      navigate(newPath + location.search, { replace: true });
+    }
+  }, [location.pathname, location.search, navigate]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigate replace to="/login" />} />
-      {/*Rutas*/}
       {routesConfig.map(({ path, element }) => (
         <Route key={path} path={path} element={element} />
       ))}
